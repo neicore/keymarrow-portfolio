4a0d53b3b8274b89264e6fc7c09b8dfb4c2ce2bc
keymarrow <pkimaro11@gmail.com>
2022-10-23 15:58:43 +0300

    first react portflio

---
+++ src/components/Skills/Edit.jsx
@@ -0,0 +1,71 @@
+import React from 'react'
+
+const Editing = () => {
+  return (
+    <div className="skills_content">
+    <h3 className="skills_title">Visual Editor</h3>
+
+    <div className="skills_box">
+        <div className="skills_group">
+            <div className="skills_data">
+                <i class='bx bx-badge-check'></i>
+
+                <div>
+                    <h3 className="skills_name">Photoshop</h3>
+                    <span className="skills_level">Advanced</span>
+                </div>
+            </div>
+
+            <div className="skills_data">
+                <i class='bx bx-badge-check'></i>
+
+                <div>
+                    <h3 className="skills_name">Adobe XD</h3>
+                    <span className="skills_level">intermediate</span>
+                </div>
+            </div>
+
+            <div className="skills_data">
+                <i class='bx bx-badge-check'></i>
+
+                <div>
+                    <h3 className="skills_name">AfterEffects</h3>
+                    <span className="skills_level">Basic</span>
+                </div>
+            </div>
+        </div>
+
+        <div className="skills_group">
+            <div className="skills_data">
+                <i class='bx bx-badge-check'></i>
+
+                <div>
+                    <h3 className="skills_name">Illustrator</h3>
+                    <span className="skills_level">Basic</span>
+                </div>
+            </div>
+
+            <div className="skills_data">
+                <i class='bx bx-badge-check'></i>
+
+                <div>
+                    <h3 className="skills_name">Figma</h3>
+                    <span className="skills_level">Advanced</span>
+                </div>
+            </div>
+
+            <div className="skills_data">
+                <i class='bx bx-badge-check'></i>
+
+                <div>
+                    <h3 className="skills_name">Blender</h3>
+                    <span className="skills_level">Beginner</span>
+                </div>
+            </div>
+        </div>
+    </div>
+</div>
+  );
+};
+
+export default Editing;
